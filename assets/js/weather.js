6aa51a72e02f4bc4d178c694d01cf9908e2b3f2a
mailahnheidi@gmail.com <mailahnheidi@gmail.com>
2022-01-06 21:00:17 -0600

    started JavaScript for weather search

---
+++ assets/js/weather.js
@@ -0,0 +1,11 @@
+var searchBtnEl = document.querySelector("#search-btn");
+var cityInput
+
+var formSubmitHandler = function (event) {
+// prevent page from refreshing
+event.preventDefault();
+
+};
+
+searchBtnEl.addEventListener("submit", formSubmitHandler);
+
